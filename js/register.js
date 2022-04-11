c3fec0d67aa17f0e1b0d37f55245657a64883518
Dương Phạm <pdtduong03@gmail.com>
2022-04-11 17:05:06 +0700

    Commit "Fix Register Login"

--- js/register.js
+++ js/register.js
@@ -19,9 +19,10 @@ window.addEventListener("load", function () {
   let isPwd = false;
 
   form.onsubmit = (e) => {
+    console.log(gender.value);
     e.preventDefault();
     if (username.value == "") {
-      error1.innerText = "Bạn cần nhập Tài Khoản, tối thiểu 5 ký tự";
+      error1.innerText = "Bạn cần nhập Tài Khoản, tối thiểu 10 ký tự";
       username.classList.add("invalid");
     }
     if (password.value == "") {
@@ -32,79 +33,22 @@ window.addEventListener("load", function () {
       error3.innerText = "Bạn cần nhập đúng mật khẩu đã nhập";
       rePassword.classList.add("invalid");
     }
-    if (gender.value == "") {
-      error4.innerText = "Bạn cần nhập đúng mật khẩu đã nhập";
+    if (gender.value == 0) {
+      error4.innerText = "Bạn cần chọn giới tính";
       gender.classList.add("invalid");
     }
     if (displayName.value == "") {
-      error5.innerText = "Bạn cần nhập tên Hiển thị";
+      error5.innerText = "Bạn cần nhập tên hiển thị";
       displayName.classList.add("invalid");
     }
     if (email.value == "") {
       error6.innerText = "Bạn cần nhập email";
       email.classList.add("invalid");
-      if(email.valie !== ""){
-        const pattern =
-        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if (!pattern.test(email.value)) {
-        error6.innerText = "Bạn cần nhập đúng định dạng email";
-        email.classList.add("invalid");
-      } else {
-          email.classList.remove("invalid");
-          error6.innerText = "";
-      }
-      }
     }
     if (phone.value == "") {
       error7.innerText = "Bạn cần nhập Số điện thoại";
       phone.classList.add("invalid");
     }
-
-    username.onkeyup = () => {
-      if (username.value !== "") {
-        error1.innerText = "";
-        username.classList.remove("invalid");
-      }
-    };
-
-    password.onkeyup = () => {
-      if (password.value !== "") {
-        error2.innerText = "";
-        password.classList.remove("invalid");
-      }
-    };
-
-    rePassword.onkeyup = () => {
-      if (rePassword.value !== "") {
-        error3.innerText = "";
-        rePassword.classList.remove("invalid");
-      }
-    };
-    gender.onkeyup = () => {
-      if (gender.value !== "") {
-        error4.innerText = "";
-        gender.classList.remove("invalid");
-      }
-    };
-    displayName.onkeyup = () => {
-      if (displayName.value !== "") {
-        error5.innerText = "";
-        displayName.classList.remove("invalid");
-      }
-    };
-    email.onkeyup = () => {
-        if (email.value !== "") {
-            error6.innerText = "";
-            email.classList.remove("invalid");
-          }
-    };
-
-    phone.onkeyup = () => {
-      if (phone.value !== "") {
-        error7.innerText = "";
-        phone.classList.remove("invalid");
-      }
-    };
 
     if (
       !username.classList.contains("invalid") &&
@@ -117,6 +61,87 @@ window.addEventListener("load", function () {
     ) {
       console.log("Form submitted");
       alert("Đăng ký thành công");
+    }else{
+      alert("Đăng ký thất bại");
     }
   };
+
+  function check(){
+    username.onkeyup = () => {
+      if (username.value !== "") {
+        if(username.value.length < 10){
+          error1.innerText = "Tài khoản phải từ 10 ký tự trở lên";
+          username.classList.add("invalid");
+        }else{
+          error1.innerText = "";
+          username.classList.remove("invalid");
+        }
+      }
+    };
+
+    password.onkeyup = () => {
+      if (password.value !== "") {
+        if(password.value.length < 10){
+          error2.innerText = "Mật khẩu phải từ 10 ký tự trở lên";
+          password.classList.add("invalid");
+        }else{
+          error2.innerText = "";
+          password.classList.remove("invalid");
+        }
+      }
+    };
+
+    rePassword.onkeyup = () => {
+      if (rePassword.value !== "") {
+        if(rePassword.value !== password.value){
+          error3.innerText = "Bạn cần nhập đúng mật khẩu đã nhập";
+          rePassword.classList.add("invalid");
+        }
+        else{
+          error3.innerText = "";
+          rePassword.classList.remove("invalid");
+        }
+      }
+    };
+    gender.onmousedown = () => {
+      if (gender.value !== "") {
+        error4.innerText = "";
+        gender.classList.remove("invalid");
+      }
+    };
+    displayName.onkeyup = () => {
+      if (displayName.value !== "") {
+        error5.innerText = "";
+        displayName.classList.remove("invalid");
+      }
+    };
+    email.onkeyup = () => {
+      if (email.value !== "") {
+        const pattern =
+          /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!pattern.test(email.value)) {
+          error6.innerText = "Bạn cần nhập đúng định dạng email";
+          email.classList.add("invalid");
+        } else {
+          email.classList.remove("invalid");
+          error6.innerText = "";
+        }
+      }
+    };
+
+    phone.onkeyup = () => {
+      if (phone.value !== "") {
+        const rule = /^[0-9]+$/;
+        if(!rule.test(phone.value)){
+          error7.innerText = "Bạn cần nhập đúng định dạng số điện thoại 0-9";
+          phone.classList.add("invalid");
+        }else{
+          error7.innerText = "";
+          phone.classList.remove("invalid");
+        }
+      }
+    };
+  }
+
+  check();
 });
